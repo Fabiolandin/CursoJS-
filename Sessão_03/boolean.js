b75a9fbd1a46ec18c7abc5f8289a2c6f107029ec
Fabio Landin <flandin1990@gmail.com>
2026-02-09 20:52:28 -0300

    Aula sobre tipo boolean

---
+++ Sessão_03/boolean.js
@@ -0,0 +1,11 @@
+//o tipo boolean é um dos tipos primitivos de js
+//eles podem ser/retornar apenas "true" ou "false"
+
+let isTheSunAStar = true;
+let isTheEartFlat = false;
+
+let result1 = 10 > 5; //true
+let result2 = 10 < 5; //false
+
+console.log(result1)
+console.log(result2)
