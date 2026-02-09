656e065be7260f15b191485ef1cf8695ea439ead
Fabio Landin <flandin1990@gmail.com>
2026-02-09 20:44:42 -0300

    Aula sobre tipo boolean

---
+++ Sessão_03/number.js
@@ -0,0 +1,16 @@
+//Um dos tipos mais comuns usados é o number
+//number é usado para representar números inteiros e decimais, como 10, 3.14, -5 e etc.
+
+let myNumber1 = 10; //inteiro
+let myNumber2 = 3.14; //decimal
+
+//Usaremos muito para realizar operações matematicas, com numeros positivos negativos e zerados
+
+let myNumber3 = 10;
+let myNumber4 = -10;
+let myNumber5 = 0;
+
+//Em raras ocasiões usaremos o tipo 'bigint' para representar números muito grandes
+//o sufixo n é necessario para indicar que o valor é um numero 'bigint'
+let myBigint = 123456789012345678901234567890n;
+
