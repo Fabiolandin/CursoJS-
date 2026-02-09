dbe02e143bdc35346bd67165089de97f3b1267b3
Fabio Landin <flandin1990@gmail.com>
2026-02-09 20:57:40 -0300

    Aula sobre null e undefined

---
+++ Sessão_03/null_undefined.js
@@ -0,0 +1,13 @@
+//Tipos de dados 'null' e 'undefined' são dois tipos de dados primitivos em javascript.
+//os dois representam a ausencia de valor porém só um é recomendado
+
+let myNull = null; //recomendado
+console.log(myNull); // null
+
+let myUndefined = undefined; //udefined pode ser atribuido a uma variavel, mas NÃO É RECOMENDADO
+console.log(myUndefined)
+
+// Undefined significa que ainda não foi atribuido um valor á variavel
+
+let myNumber; //nenhum valor atribuido
+console.log(myNumber); //undefined
