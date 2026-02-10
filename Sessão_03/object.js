32312b36ddec50f566f33c8e42194de449f86a33
Fabio Landin <flandin1990@gmail.com>
2026-02-09 21:53:38 -0300

    Aula sobre object e typeof

---
+++ Sessão_03/object.js
@@ -0,0 +1,17 @@
+//Um dos tipos de dados mais importantes de JS é o 'object'
+//objetos são estruturas de dados que podem armazenar coleções de dados e funcionalidades.
+
+//definindo um objeto
+let myObject = {
+    name: 'Mike',    //propriedade nome
+    age: '31',       //propriedade idade
+    isMarried: false //propriedade casado ou não
+};
+
+//Apresentando o objeto completo
+console.log(myObject)
+
+//Apresentando somente a propriedade selecionada
+console.log(myObject.name)
+console.log(myObject.age)
+console.log(myObject.isMarried)
---
+++ Sessão_03/typeof.js
@@ -0,0 +1,15 @@
+//Typeof é um operador do JavaScript que retorna o tipo de variavel ou expressão.
+
+let myString = 'Hello World!';
+let myNumber = 100;
+let myBoolean = true;
+let myObject = {
+    name: 'Mike',    
+    age: '31',       
+    isMarried: false 
+};
+
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
+console.log(typeof myObject);
