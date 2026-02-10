2f7816bf1d5df2a5f4321bf731f2891eb7d7f261
Fabio Landin <flandin1990@gmail.com>
2026-02-10 20:28:18 -0300

    Primeira lista de exercicios

---
+++ Sessão_03/exercicios4.js
@@ -0,0 +1,36 @@
+//crie uma variavel chamada factor, crie um array com 10 numeros, essa deve ser a tabuada da variavel factor
+//o resultado deve ser exibido em forma de tabela
+
+let factor = 2;
+let tabuada = [ 
+    factor * 1,
+    factor * 2,
+    factor * 3,
+    factor * 4,
+    factor * 5,
+    factor * 6,
+    factor * 7,
+    factor * 8,
+    factor * 9
+
+]
+console.table(tabuada)
+
+//Concatene os valores da tabuada2 e exiba o resultado
+let factor2 = 2;
+let tabuada2 = [ 
+    factor2 + " X 1 = "+ factor2 * 1,
+    factor2 + " X 2 = "+ factor2 * 2,
+    factor2 + " X 3 = "+ factor2 * 3,
+    factor2 + " X 4 = "+ factor2 * 4,
+    factor2 + " X 5 = "+ factor2 * 5,
+    factor2 + " X 6 = "+ factor2 * 6,
+    factor2 + " X 7 = "+ factor2 * 7,
+    factor2 + " X 8 = "+ factor2 * 8,
+    factor2 + " X 9 = "+ factor2 * 9,
+
+]
+console.table(tabuada2)
+
+
+
