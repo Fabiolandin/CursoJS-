b72b9f29f068e2fd22e05da8fcd12edf350eae7e
Fabio Landin <flandin1990@gmail.com>
2026-02-10 20:06:12 -0300

    Aula sobre operadores aritmeticos

---
+++ Sessão_03/operadores_aritmeticos.js
@@ -0,0 +1,31 @@
+//Os operadores aritmeticos são usados para realizar operações matematicas em javascript
+// + (adição), - (Subtração), / (Divisão), *(Multiplicação), %(Módulo), **(Exponenciação).
+
+let num1 = 10;
+let num2 = 5;
+
+//Adição
+let soma = num1 + num2;
+
+//Subtração
+let subtracao = num1 - num2;
+
+//Divisão
+let divisao = num1 / num2;
+
+//Multiplicação
+let multiplicacao = num1 * num2;
+
+//Módulo
+let modulo = num1 % num2; // 10 % 5 = 0
+let modulo2 = 11 % 5;     // 11 % 5 = 1
+
+//Exponenciação
+let exponenciacao = num1 ** num2; // 10 ** 5 = 100000
+
+//Operadores Unarios
+let numUn = 10;
+
+numUn++; //11
+numUn--; //10
+
