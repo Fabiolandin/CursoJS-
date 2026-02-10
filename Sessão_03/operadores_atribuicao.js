2f8bd6d232bd60091a19d08ee4bb411356ed0502
Fabio Landin <flandin1990@gmail.com>
2026-02-10 20:10:36 -0300

    Aula sobre operadores de atribuicao

---
+++ Sessão_03/operadores_atribuicao.js
@@ -0,0 +1,22 @@
+// Os operadores de atribuição são usados para atribuir valores a variáveis.
+// Eles podem ser usados para atribuir valores simples ou realizar operações matemáticas
+// e atribuir o resultado a uma variável. Os operadores de atribuição mais comuns são:
+
+// O operador de atribuição é o símbolo igual (=). Ele atribui o valor à variável à esquerda do operador.
+let num = 10;
+
+// Operator de atribuição com adição.
+num += 2;    // o operador += adiciona 2 unidades à variável num
+// como se dissesse: num = num + 2
+
+// Operator de atribuição com subtração
+num -= 2;    // o operador -= subtrai 2 unidades da variável num
+// como se dissesse: num = num - 2
+
+// Operator de atribuição com multiplicação
+num *= 2;    // o operador *= multiplica a variável num por 2
+// como se dissesse: num = num * 2
+
+// Operator de atribuição com divisão
+num /= 2;    // o operador /= divide a variável num por 2
+// como se dissesse: num = num / 2
