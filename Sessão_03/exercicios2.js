34bd0b567875d65000770cb91f97266ee006f46c
Fabio Landin <flandin1990@gmail.com>
2026-02-09 22:02:59 -0300

    Exercicios 2 curso

---
+++ Sessão_03/exercicios2.js
@@ -0,0 +1,7 @@
+/*
+Defina 3 variaveis numéricas, uma inteiro, uma decimal e defina uma variavel result que calcula os dois
+*/
+let myNumber1 = 10;
+let myNumber2 = 3.4;
+let result = myNumber1 + myNumber2;
+console.log(result);
