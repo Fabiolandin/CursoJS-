3c5ed4d5acd56fd4bb84d1b2bfb5fa594b5285ef
Fabio Landin <flandin1990@gmail.com>
2026-02-10 20:45:48 -0300

    Aula sobre operadores de comparação

---
+++ Sessão_03/operadores_comparacao.js
@@ -0,0 +1,20 @@
+//Os operadores de comparação são usados para comparar dois valores
+//Eles retornam true ou false dependendo do resultado da comparação.
+
+let num_1 = 5;  
+let num_2 = 10;
+
+// operador de comparação (==)
+console.log(num_1 == num_2);    // false
+
+// operador de negação de comparação (!=)
+console.log(num_1 != num_2);    // true
+
+// operador de comparação maior que (>)
+console.log(num_1 > num_2);    // false
+
+// operador de comparação maior ou igual que (>=)
+console.log(num_1 >= num_2);    // false
+
+// operador de comparação menor que (<)
+console.log(num_1 < num_2);    // true
