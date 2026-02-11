7e40d7b842acb5912798060bc6bcaa12ad48f371
Fabio Landin <flandin1990@gmail.com>
2026-02-11 09:04:32 -0300

    Aula sobre bloco de código

---
+++ Sessão_03/bloco_codigo.js
@@ -0,0 +1,17 @@
+//O bloco de código é um conjunto de instruções agrupadas entre chaves {}.
+//Serve para organizar o código e definir o escopo
+
+//Uma instrução if fica em um bloco de código
+if (true) {
+    console.log('This is true');
+}
+
+//Um loop for com um bloco de código
+for (let i = 0; i < 10; i++ ) {
+    console.log(i)
+}
+
+//Uma função com um bloco de código
+function myFunction() {
+    console.log('This is a function');
+}
