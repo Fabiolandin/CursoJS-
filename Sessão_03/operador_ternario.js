64ff65c1e5b0e3cc1f19af31f24b84d3283d0905
Fabio Landin <flandin1990@gmail.com>
2026-02-11 14:01:27 -0300

    Operador ternario

---
+++ Sessão_03/operador_ternario.js
@@ -0,0 +1,11 @@
+//Operador ternario é uma forma mais simples de usar o if e else em JavaScript.
+//É também conhecido como operador condicional ou operador de condição
+//(comparação), se sim (resposta), se não (resposta)
+
+let myName = 'Jhon';
+console.log(myName === 'Jhon' ? 'Hellow Jhon' : 'Hellow Visitante'); //Hellow Jhon
+
+myName = 'Jane';
+console.log(myName === 'Jhon' ? 'Hellow Jhon' : 'Hellow Visitante'); //Hellow Visitante
+
+
