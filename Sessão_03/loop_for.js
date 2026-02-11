f8fe8290824f949831041849bff97e2a66b8cb24
Fabio Landin <flandin1990@gmail.com>
2026-02-11 20:49:30 -0300

    Aulas sobre loops for e while

---
+++ Sessão_03/loop_for.js
@@ -0,0 +1,14 @@
+//O loop for é uma estrutura de repetição que permite executar um bloco de código varias vezes
+
+
+//esse loop ira imprimir do 0 ao 9
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+//Loop para percorrer linhas e colunas [Matrizes]
+for (let row = 0; row < 3; row++){
+    for (let column = 0; column < 3; column++) {
+        console.log(row, column);
+    }
+}
---
+++ Sessão_03/loop_while.js
@@ -0,0 +1,18 @@
+//O loop while executa um bloco de código enquanto a condição for verdedeira.
+
+let index = 0;
+
+while (index < 10){
+    console.log(index);
+    index++;
+}
+
+//Para imprimir somente numeros pares
+let cont = 0;
+
+while (cont < 10) {
+    if (cont % 2 === 0) {
+        console.log(cont);
+    }
+    cont++;
+}
