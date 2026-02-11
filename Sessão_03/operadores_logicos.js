7407e92adedd6995759f3e53e26d3cf310c4f3a5
Fabio Landin <flandin1990@gmail.com>
2026-02-11 11:04:11 -0300

    Finalizando segunda aula sobre operadores lógicos

--- Sessão_03/operadores_logicos.js
+++ Sessão_03/operadores_logicos.js
@@ -17,3 +17,27 @@ console.log(false || false);  //
 //NOT (!)
 console.log(!true);   //false
 console.log(!false);  //true
+
+//Exemplo prático (AND) = E
+let age = 80;
+
+if (age >= 18 && age <= 65){ //maior ou igual a 18 anos e menor ou igual a 65
+    console.log('Pode dirigir');
+} else {
+    console.log('Não pode dirigir!');
+}
+
+//Exemplo prático (OR) = OU
+if (age < 18 || age > 65){
+    console.log('Não pode dirigir');
+} else {
+    console.log('Pode dirigir!')
+}
+
+//Exemplo prático de (NOT) = diferente
+let isUser = true;
+if (!isUser) {
+    console.log('Voce precisa estar logado!')
+} else {
+    console.log('Bem vindo á pagina!')
+}
