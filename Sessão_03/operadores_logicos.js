d3c14d9530af21f9e99e63eeba101049c164add0
Fabio Landin <flandin1990@gmail.com>
2026-02-11 08:30:09 -0300

    Aula sobre operadores logicos

---
+++ Sessão_03/operadores_logicos.js
@@ -0,0 +1,19 @@
+//Os operadores lógicos são usados para combinar expressões booleanas.
+//Eles são frequentemente usados em estruturas de controle, como if, while e for
+
+
+// AND (&&)
+console.log(true && true);    //True
+console.log(true && false);   //false
+console.log(false && true);   //false
+console.log(false && false);  //false
+
+// OR (||)
+console.log(true || true);    //
+console.log(true || false);   //
+console.log(false || true);   //
+console.log(false || false);  //
+
+//NOT (!)
+console.log(!true);   //false
+console.log(!false);  //true
