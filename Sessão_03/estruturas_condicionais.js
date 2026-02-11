02af19a4b9f4177b2b382218d2edb1feccd58a5e
Fabio Landin <flandin1990@gmail.com>
2026-02-11 08:54:30 -0300

    Aula sobre estruturas condicionais

---
+++ Sessão_03/estruturas_condicionais.js
@@ -0,0 +1,17 @@
+//Estruturas condicionais: if....else...
+
+let temperatura = 20;
+
+console.log("Vamos ver como está o tempo!");
+
+//Instrução IF simples
+if (temperatura == 20){
+    console.log("Estão 20 graus!");
+}
+
+//Instrução if....else
+if (temperatura <= 0){
+    console.log("Está abaixo de zero!");
+} else {
+    console.log("Está acima de zero!");
+}
