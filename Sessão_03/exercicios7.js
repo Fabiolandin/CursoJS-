70afb3a3929d21d05962e935ee00ddc9f66b6d1f
Fabio Landin <flandin1990@gmail.com>
2026-02-11 21:15:32 -0300

    Exercicios sobre loops

---
+++ Sessão_03/exercicios7.js
@@ -0,0 +1,11 @@
+// Cire um loop para exibir uma tabuada
+for (let num = 1; num < 11; num++){
+    console.log(num+ " X 6 = "+ num * 6)
+}
+
+//Crie um number = 1000, o loop = 10, cada iteração do loop number precisa receber 100
+let number = 1000;
+for (let contador = 1; contador < 11; contador++){
+    number += 100
+    console.log(number)
+}
