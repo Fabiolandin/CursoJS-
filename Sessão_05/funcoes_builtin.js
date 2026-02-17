94a3e7332008b91ae6bfac43f4056d309236bcee
Fabio Landin <flandin1990@gmail.com>
2026-02-17 10:15:35 -0300

    Aula sobre funções built-in

---
+++ Sessão_05/funcoes_builtin.js
@@ -0,0 +1,23 @@
+//O JavaScript possui funções integradas que podem ser usadas para realizar perações comuns.
+//Estas funções são parte do objeto global. e podem ser usadas sem a necessidade de declarar ou importar nada.
+
+//Funções Matemáticas
+console.log(Math.PI);
+console.log(Math.round(4.7));
+console.log(Math.pow(8, 2));
+console.log(Math.sqrt(64));
+
+//Funções de Data
+const date = new Date();
+
+//Funções de data
+console.log(date); //data atual
+console.log(date.getFullYear()) //exibe o ano atual
+console.log(date.getMonth()) //exibe o mes atual
+console.log(date.getDay());
+
+//Funçõesdo console
+console.log('Hello World');
+console.log('Error!');
+console.log('Warning!');
+console.log('Information!');
